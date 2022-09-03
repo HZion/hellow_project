a8737f29cfdd4faffaf1461da9d7d57adbe45d25
sion <hasion96@gmail.com>
2022-09-03 17:39:22 +0900

    make list view

--- src/main/java/CRUD.java
+++ src/main/java/CRUD.java
@@ -21,7 +21,12 @@ public class CRUD implements ICRUDinterface {
 
     }
     public void viewList(){
-
+        System.out.println("----------------------------------");
+        for(int i = 0; i< list.size(); i++){
+            System.out.print((i+1)+ " ");
+            System.out.println(list.get(i).toString());
+        }
+        System.out.println("----------------------------------");
     }
     public int update(Object obj){
         return 0;
